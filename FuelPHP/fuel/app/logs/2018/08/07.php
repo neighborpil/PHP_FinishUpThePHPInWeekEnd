f021042bc6541dfe8ff2e2bf2cc5fddd00ae541c
neighborpil <neighborpil@naver.com>
2018-08-07 07:33:55 +0900

    making ask board

---
+++ FuelPHP/fuel/app/logs/2018/08/07.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-07 06:58:54 --> Parsing Error - syntax error, unexpected T_DOUBLE_ARROW in /mnt/fuelphp/fuel/app/config/db.php on line 11
+ERROR - 2018-08-07 07:15:31 --> Fatal Error - Class 'Orm\Model' not found in /mnt/fuelphp/fuel/app/classes/model/request.php on line 5
+ERROR - 2018-08-07 07:32:56 --> shutdown - The session data stored by the application in the cookie exceeds 4Kb. Select a different session storage driver. in /mnt/fuelphp/fuel/core/classes/session/driver.php on 501
+ERROR - 2018-08-07 07:33:17 --> shutdown - The session data stored by the application in the cookie exceeds 4Kb. Select a different session storage driver. in /mnt/fuelphp/fuel/core/classes/session/driver.php on 501
